18563506c1c679e2ab503e50025502213b2bd9b8
jtabach <jeff.n.tabachnick@gmail.com>
2018-12-10 18:18:37 +0900

    initial commit

---
+++ index.js
@@ -0,0 +1,23 @@
+var express = require('express');
+var path = require('path');
+var logger = require('morgan');
+var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
+
+var routes = require('./routes/index');
+
+var app = express();
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'jade');
+
+app.use(bodyParser.json({ limit: '50mb' }));
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
+
+app.use('/', routes);
+
+const PORT = process.env.PORT || 5001;
+
+app.listen(PORT);
